d9fb1c5329277de8bd2aaf073719e946d9624da7
seung-ho back <ilikeapink@naver.com>
2024-06-16 11:50:16 +0000

    Data_Structure

--- 윤성우의_열혈_자료구조/Ch1)자료구조와_알고리즘의_이해/LinearSearch.c
+++ 윤성우의_열혈_자료구조/Ch1)자료구조와_알고리즘의_이해/LinearSearch.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int LSearch(int ar[], int len, int target){
+int LSearch(int ar[], int len, int target){     // 순차 탐색 알고리즘 적용된 함수
     int i;
     for(i = 0; i < len; i++){
         if(ar[i] == target){
-            return i;
+            return i;       // 찾은 대상의 인덱스 값 반환
         }
     }
-    return -1;
+    return -1;      // 찾지 못했음을 의미하는 값 반환
 }
 
 int main(){
